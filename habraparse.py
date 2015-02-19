3d2b7c9f89bbbc594b8b04046f9a8ab35ad5575a
Volodymyr.Korniichuk <9173519@gmail.com>
2015-02-19 11:31:52 +0200

    images get implemented

--- habraparse.py
+++ habraparse.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import requests
 import re
+import os
+from time import sleep
 
 def get_topic_images(topic_id):
     req = requests.get('http://habrahabr.ru/post/' + str(topic_id) + '/')
@@ -13,19 +14,25 @@ def get_topic_images(topic_id):
     for image in all_images:
         if image.find('//habrastorage.org/') != -1 or image.find('mc.yandex.ru/watch') != -1 or image.find('ad.adriver.ru') != -1:
             continue
-        if image.find('"/images/') != -1 or image.find('http://habrahabr.ru/i/') != -1:
+        if image.find('/images/') == 0 or image.find('http://habrahabr.ru/i/') != -1 or image.find('http://geektimes.ru/i/') != -1 or image.find('http://megamozg.ru/i/') != -1:
             continue
         result.add(image)
     return result
 
 def get_images_for_topics_range(start_topic_id, end_topic_id):
-    images = set()
     for i in range(start_topic_id, end_topic_id):
-        images.update(get_topic_images(i))
-    print(images)
+        current_topic_images = get_topic_images(i)
+        for j in current_topic_images:
+            ulr_info = str(i) + " " + j
+            print(ulr_info);
+            with open('img_info\\' + str(int(i / 10000)), "a") as f:
+                f.write(ulr_info + '\n')
+        sleep(0.5)
 
 def main():
-    get_images_for_topics_range(61884, 61885)
+    if not os.path.exists('img_info'):
+        os.makedirs('img_info')
+    get_images_for_topics_range(1, 188436) #188436 - info about movement of all new images to habrastorage.org
 
 if __name__ == '__main__':
     main()
